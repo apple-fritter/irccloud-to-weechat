4e1734a76a8d05c9c394f72283693b0bec2342f9
Ａｐｐｌｅ Ｆｒｉｔｔｅｒ <125221355+apple-fritter@users.noreply.github.com>
2023-06-11 16:54:12 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,100 @@
 use std::env;
-use std::fs::File;
-use std::io::{BufRead, BufReader, Result, Write};
+use std::fs::{self, File};
+use std::io::{BufRead, BufReader, Write};
+use std::path::{Path, PathBuf};
+use zip::ZipArchive;
 
 struct WeechatLine {
-    date: String,
-    time: String,
-    nick: String,
-    msg: String,
-}
-
-impl WeechatLine {
-    fn new(date: String, time: String, nick: String, msg: String) -> Self {
-        WeechatLine {
-            date,
-            time,
-            nick,
-            msg,
-        }
-    }
-}
-
-fn parse_line(line: &str) -> Option<WeechatLine> {
-    let parts: Vec<&str> = line.split('\t').collect();
-    if parts.len() == 3 {
-        let timestamp = parts[0];
-        let nick = parts[1];
-        let msg = parts[2];
-
-        let datetime_parts: Vec<&str> = timestamp.split('T').collect();
-        if datetime_parts.len() == 2 {
-            let date = datetime_parts[0];
-            let time = datetime_parts[1];
-
-            Some(WeechatLine::new(date.to_string(), time.to_string(), nick.to_string(), msg.to_string()))
-        } else {
-            None
-        }
-    } else {
-        None
-    }
-}
-
-fn convert_log(input_path: &str, output_path: &str) -> Result<()> {
-    let input_file = File::open(input_path)?;
-    let reader = BufReader::new(input_file);
-
-    let weechat_lines: Vec<_> = reader
-        .lines()
-        .filter_map(|line| line.ok())
-        .filter_map(|line| parse_line(&line))
-        .collect();
-
-    let output_file = File::create(output_path)?;
-    let mut writer = std::io::BufWriter::new(output_file);
-
-    for line in weechat_lines {
-        writeln!(writer, "{} {} <{}> {}", line.date, line.time, line.nick, line.msg)?;
-    }
-
-    Ok(())
+    timestamp: String,
+    sender: String,
+    message: String,
 }
 
 fn main() {
+    // Read the command-line arguments
     let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        eprintln!("Usage: {} <input_path> <output_path>", args[0]);
+    if args.len() != 2 {
+        println!("Usage: irccloud-to-weechat <input_zip>");
         return;
     }
 
-    let input_path = &args[1];
-    let output_path = &args[2];
+    // Extract the input file path from the arguments
+    let input_zip = &args[1];
 
-    if let Err(err) = convert_log(input_path, output_path) {
-        eprintln!("Error: {}", err);
-    } else {
-        println!("Conversion completed successfully.");
+    // Open the zip archive
+    let archive = match ZipArchive::new(File::open(input_zip)?) {
+        Ok(archive) => archive,
+        Err(err) => {
+            println!("Failed to open zip archive: {}", err);
+            return;
+        }
+    };
+
+    // Process each file in the archive
+    for i in 0..archive.len() {
+        let entry = match archive.by_index(i) {
+            Ok(entry) => entry,
+            Err(err) => {
+                println!("Failed to read entry: {}", err);
+                continue;
+            }
+        };
+
+        // Extract the file name and extension
+        let file_name = entry.name();
+        let path = Path::new(file_name);
+        let file_name_stem = path.file_stem().unwrap().to_str().unwrap();
+        let output_file_name = format!("{}.weechatlog", file_name_stem);
+
+        // Create the output file
+        let output_file = match File::create(output_file_name) {
+            Ok(file) => file,
+            Err(err) => {
+                println!("Failed to create output file: {}", err);
+                continue;
+            }
+        };
+
+        let output_path = PathBuf::from(&output_file_name);
+
+        // Process the lines of the input file
+        let reader = BufReader::new(entry);
+        let weechat_lines: Vec<WeechatLine> = reader
+            .lines()
+            .filter_map(|line| parse_line(&line.ok()?, &output_path))
+            .collect();
+
+        // Write the Weechat-formatted lines to the output file
+        let mut output_writer = BufWriter::new(output_file);
+        for weechat_line in weechat_lines {
+            writeln!(
+                output_writer,
+                "{} {} {}",
+                weechat_line.timestamp,
+                weechat_line.sender,
+                weechat_line.message
+            )
+            .unwrap();
+        }
     }
+
+    println!("Conversion completed successfully!");
+}
+
+fn parse_line(line: &str, output_path: &Path) -> Option<WeechatLine> {
+    // Extract the date, time, sender, and message from the line
+    let parts: Vec<&str> = line.splitn(3, ' ').collect();
+    if parts.len() < 3 {
+        return None;
+    }
+
+    let timestamp = parts[0];
+    let sender = parts[1];
+    let message = parts[2];
+
+    Some(WeechatLine {
+        timestamp: timestamp.to_owned(),
+        sender: sender.to_owned(),
+        message: message.to_owned(),
+    })
 }
