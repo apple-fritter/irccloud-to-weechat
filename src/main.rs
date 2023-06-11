b7d0523786ebebd7f8468b6224535959d1b90f4d
Ａｐｐｌｅ Ｆｒｉｔｔｅｒ <125221355+apple-fritter@users.noreply.github.com>
2023-06-11 16:49:06 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,79 @@
+use std::env;
 use std::fs::File;
-use std::io::{BufReader, BufRead};
-use std::path::Path;
-use zip::read::ZipArchive;
+use std::io::{BufRead, BufReader, Result, Write};
+
+struct WeechatLine {
+    date: String,
+    time: String,
+    nick: String,
+    msg: String,
+}
+
+impl WeechatLine {
+    fn new(date: String, time: String, nick: String, msg: String) -> Self {
+        WeechatLine {
+            date,
+            time,
+            nick,
+            msg,
+        }
+    }
+}
+
+fn parse_line(line: &str) -> Option<WeechatLine> {
+    let parts: Vec<&str> = line.split('\t').collect();
+    if parts.len() == 3 {
+        let timestamp = parts[0];
+        let nick = parts[1];
+        let msg = parts[2];
+
+        let datetime_parts: Vec<&str> = timestamp.split('T').collect();
+        if datetime_parts.len() == 2 {
+            let date = datetime_parts[0];
+            let time = datetime_parts[1];
+
+            Some(WeechatLine::new(date.to_string(), time.to_string(), nick.to_string(), msg.to_string()))
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
+
+fn convert_log(input_path: &str, output_path: &str) -> Result<()> {
+    let input_file = File::open(input_path)?;
+    let reader = BufReader::new(input_file);
+
+    let weechat_lines: Vec<_> = reader
+        .lines()
+        .filter_map(|line| line.ok())
+        .filter_map(|line| parse_line(&line))
+        .collect();
+
+    let output_file = File::create(output_path)?;
+    let mut writer = std::io::BufWriter::new(output_file);
+
+    for line in weechat_lines {
+        writeln!(writer, "{} {} <{}> {}", line.date, line.time, line.nick, line.msg)?;
+    }
+
+    Ok(())
+}
 
 fn main() {
-    let file = File::open("irccloud_logs.zip").unwrap();
-    let mut archive = ZipArchive::new(file).unwrap();
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        eprintln!("Usage: {} <input_path> <output_path>", args[0]);
+        return;
+    }
 
-    for i in 0..archive.len() {
-        let mut file = archive.by_index(i).unwrap();
-        let path = Path::new(file.name());
+    let input_path = &args[1];
+    let output_path = &args[2];
 
-        // Skip any files that are not log files
-        if !path.extension().unwrap().to_str().unwrap().eq("txt") {
-            continue;
-        }
-
-        let reader = BufReader::new(file);
-        let mut output = File::create(format!("{}.weechatlog", path.file_stem().unwrap().to_str().unwrap())).unwrap();
-
-        for line in reader.lines() {
-            let line = line.unwrap();
-            let parts: Vec<&str> = line.splitn(3, ' ').collect();
-
-            // Skip any lines that do not contain enough information
-            if parts.len() < 3 {
-                continue;
-            }
-
-            let timestamp = format!("{}:{}:{}", &parts[0][0..2], &parts[0][3..5], &parts[0][6..8]);
-            let message = format!("[{}] {}", timestamp, &parts[2]);
-
-            // Write the message to the output file
-            writeln!(output, "{}", message).unwrap();
-        }
+    if let Err(err) = convert_log(input_path, output_path) {
+        eprintln!("Error: {}", err);
+    } else {
+        println!("Conversion completed successfully.");
     }
 }
